33fbf713fddea6c6aba1201dc3f865a76665a027
itaygoz <itaygoz9@gmail.com>
2024-08-19 21:32:58 +0300

    feat(mongodb): establish mongodb connection

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
+import { MongoDBConfig } from './config/configuration.types';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const { host, password, port, username } =
+          config.getOrThrow<MongoDBConfig>('mongodb');
+
+        return {
+          uri: `mongodb://${username}:${password}@${host}:${port}`,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
