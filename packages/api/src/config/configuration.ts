8528ece7af8bb86e44bfc9f60ab5436b171eab7a
Itay Goz <48314049+itaygoz@users.noreply.github.com>
2024-08-24 22:26:16 +0300

    feat(redis-cache): add redis cache-manager client (#10)

--- packages/api/src/config/configuration.ts
+++ packages/api/src/config/configuration.ts
@@ -8,6 +8,11 @@ export default (): Configuration => ({
     host: process.env.MONGODB_HOST,
     port: parseInt(process.env.MONGODB_PORT) || 27017,
   },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT) || 6379,
+    ttl: parseInt(process.env.REDIS_DEFAULT_TLL) || 60 * 2 * 60 * 1000, // default two hours ttl
+  },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret',
     signOptions: {
